53839fdf79c5f2740932be39f5ed5b10b740bed7
Sudipta Barik <bariksudipta1997@gmail.com>
2026-05-08 02:03:44 +0530

    feat: custom exception handler with consistent JSON error responses

--- postal/settings.py
+++ postal/settings.py
@@ -130,5 +130,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'postal.exceptions.custom_exception_handler',
 }
 AUTH_USER_MODEL = 'accounts.User'
