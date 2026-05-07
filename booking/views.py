d7b8c5bbc0a31fc644c833df9e7fb8acd4ff6cbf
Sudipta Barik <bariksudipta1997@gmail.com>
2026-05-08 01:08:30 +0530

    feat: add search and ordering to booking module

--- booking/views.py
+++ booking/views.py
@@ -1,20 +1,24 @@
 from rest_framework import generics
-from .models import SpeedPostBooking
-from .serializers import SpeedPostBookingSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import SpeedPostBooking
+from .serializers import SpeedPostBookingSerializer
 from .permissions import IsABPM, IsAdmin
 
 class BookingListCreateView(generics.ListCreateAPIView):
-    queryset= SpeedPostBooking.objects.all()
-    serializer_class=SpeedPostBookingSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend]
+    queryset = SpeedPostBooking.objects.all()
+    serializer_class = SpeedPostBookingSerializer
+    permission_classes = [IsABPM]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['article_number']
+    search_fields = ['article_number']
+    ordering_fields = ['booked_at', 'weight_grams']
+    ordering = ['-booked_at']
 
 class BookingDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = SpeedPostBooking.objects.all()
     serializer_class = SpeedPostBookingSerializer
     permission_classes = [IsAdmin]
     lookup_field = 'article_number'
-# Create your views here.
+
